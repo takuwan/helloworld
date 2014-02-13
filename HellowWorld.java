8d29c3b1314f2d5d6e443b1edeba81206f7a9f2d
西村 拓美 <nishimura@cyber-will.co.jp>
2014-02-13 15:37:03 +0900

    refs #2 ポリモーフィズム簡易編

--- HellowWorld.java
+++ HellowWorld.java
@@ -4,6 +4,30 @@ class HellowWorld
 {
     public static void main(String args[]){
         System.out.println("Hello World.");
+        Echo o1 = new Hello().echo();
+        Echo o2 = new GoodBye().echo();
     }
 }
 
+abstract class Echo
+{
+    abstract public Echo echo();
+}
+
+class Hello extends Echo
+{
+    public Hello echo()
+    {
+        System.out.println("Hello");
+        return this;
+    }
+}
+
+class GoodBye extends Echo
+{
+    public GoodBye echo()
+    {
+        System.out.println("GoodBye");
+        return this;
+    }
+}
